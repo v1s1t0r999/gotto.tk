2ace60eebe4eae7c27174f54d3bf49c88ab498d3
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2022-04-28 11:03:23 +0530

    Update constants.py

--- data/constants.py
+++ data/constants.py
@@ -1,8 +1,8 @@
 import os
 
 _dict = {
-'github_token' : os.getenv("GTK"),
-'repo' : os.getenv("REPO"),
-'creds' : os.getenv("CREDS"),
-'host_url': os.getenv("HOST_URL")
+'github_token' : os.getenv("GTK"),  # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+'repo' : os.getenv("REPO"), # name of the repo which is to be used as the site base
+'creds' : os.getenv("CREDS"), # ("github_username", "github_conneted_email")
+'host_url': os.getenv("HOST_URL") # the github pages site name OR the CNAME if you are connecting to any other domain
 }
