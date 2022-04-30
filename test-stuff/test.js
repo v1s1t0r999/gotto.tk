5764755f5eca4d23c3ef58194018578fadefc6c4
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2022-04-30 11:32:29 +0530

    Update test.js

--- test-stuff/test.js
+++ test-stuff/test.js
@@ -1,1 +1,16 @@
-alert("hii")
+
+//create XMLHttpRequest object
+const xhr = new XMLHttpRequest()
+//open a get request with the remote server URL
+xhr.open("GET", "https://v1s1t0r999.herokuapp.com/make?src=https://leafstuios.com&name=testt")
+//send the Http request
+xhr.send()
+
+//EVENT HANDLERS
+
+//triggered when the response is completed
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    //parse JSON datax`x
+    document.getElementById('jsonresp').innerHTML = x
+      
